80a26a8ecc3272eaa00a7bd65fd59a3dc4e9741c
Alberto <alberto.vgdd@gmail.com>
2016-09-28 21:53:34 +0200

    Velocity based movement
    -Movement "glitch"  is fixed, now every entity need a Phtysics2D
    material

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -3,23 +3,25 @@ using System.Collections;
 
 
 
-//TO DO:
+//IMPORTANT:
+//      Every Single platform type requires now a different Physics2D material. (Player may stick on walls, ask Alberto for more details)
+//      Diferent enemies may also need one of those materials
+
 
 public class PlayerMovementScript : MonoBehaviour
 {
     private Rigidbody2D m_PlayerRigidbody;
     private Animator m_PlayerAnimator;
-    //private SpriteRenderer m_PlayerSprite;
     private FeetControllerScript m_PlayerFeetScript;
     private PlayerShootScript m_PlayerShootScript;
 
     //These values change how fast/high  does the player run/jump
-    public float m_WalkSpeed;
-    public float m_JumpSpeed;
-    public float m_RunMultiplier;
+    public float m_WalkSpeedValue;
+    public float m_RunSpeedValue;
+    public float m_JumpSpeedValue;
 
-    //This manages the position displacement on X axis
-    private float m_Displacement;
+    //this variable manages the amount of horizontal velocity that the player is given
+    private float m_HorizontalVelocity;
 
     //Variables to manage jumping & falling.
     private bool m_IsOnGround;
@@ -30,13 +32,14 @@ public class PlayerMovementScript : MonoBehaviour
 
     //Variables to reverse animation speed, and change between movement animations
     private bool m_IsFacingRight;
-    private bool m_IsMoving;
     private bool m_IsRunning;
+    private float m_IsWalking; // 3 different states ---> -1, 0, 1
 
 
 
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Start ()
     {
         InitializeVariables();
 
@@ -46,23 +49,17 @@ public class PlayerMovementScript : MonoBehaviour
     {
         m_PlayerRigidbody = GetComponent<Rigidbody2D>();
         m_PlayerAnimator = GetComponent<Animator>();
-        //m_PlayerSprite = GetComponent<SpriteRenderer>();
         m_PlayerFeetScript = transform.FindChild("Feet").GetComponent<FeetControllerScript>();
         m_PlayerShootScript = transform.GetComponent<PlayerShootScript>();
 
-        m_Displacement = 0f;
 
-        m_IsMoving = false;
         m_IsJumping = false;
         m_IsOnGround = false;
         m_IsFacingRight = true;
+        m_IsWalking = 0f;
 
         m_WalkJumpBoost = 1.1f;
         m_RunJumpBoost = 1.25f;
-
-        
-
-        
 
     }
 
@@ -70,8 +67,7 @@ public class PlayerMovementScript : MonoBehaviour
     void Update ()
     {
         //Walking
-        m_Displacement = (Input.GetAxis("MovementAxisX") * m_WalkSpeed * Time.fixedDeltaTime);
-        if (m_Displacement != 0){ m_IsMoving = true; } else { m_IsMoving = false; }
+        m_IsWalking = Input.GetAxis("MovementAxisX");
 
         //Running
         if(Input.GetButton("Run") == true) { m_IsRunning = true; } else { m_IsRunning = false; }
@@ -99,21 +95,23 @@ public class PlayerMovementScript : MonoBehaviour
     }
     void Move()
     {
-        //In case the player is running, double the displacement and accelerate the animation
-        ChangeSpeed();
+        //Velocity in case either m_IsWalking or m_IsRunning are true
+        SetHorizontalVelocity();
 
-        //Check if player has to move forward or backward, then change animation speed
+        //Check if player has to move forward or backward (it depends on the mouse position), then change animation speed
         ChangeAnimationSpeed();
 
         //Change animations.
          ChangeAnimation();
 
-        m_PlayerRigidbody.position += new Vector2(m_Displacement, 0);
+        //Set the Speed
+         m_PlayerRigidbody.velocity = new Vector2(m_HorizontalVelocity, m_PlayerRigidbody.velocity.y);
+
     }
 
     void ChangeAnimationSpeed()
     {
-        if (((m_IsFacingRight && m_Displacement < 0) || (!m_IsFacingRight && m_Displacement > 0)))
+        if (((m_IsFacingRight && m_IsWalking < 0) || (!m_IsFacingRight && m_IsWalking > 0)))
         {
             m_PlayerAnimator.SetFloat("Speed", -1f);
         }
@@ -124,38 +122,49 @@ public class PlayerMovementScript : MonoBehaviour
     }
     void ChangeAnimation()
     {
-        //Change this by checking GetAxis.
-        m_PlayerAnimator.SetBool("Walk", m_IsMoving);
-    }
-    void ChangeSpeed()
-    {
-        if (m_IsRunning)
+        if (m_IsWalking !=0)
         {
-            m_Displacement *= m_RunMultiplier;
-            m_PlayerAnimator.speed = 1.5f;
+            m_PlayerAnimator.SetBool("Walk", true);
         }
         else
         {
-            m_PlayerAnimator.speed = 1f;
+            m_PlayerAnimator.SetBool("Walk", false);
         }
+        
+    }
 
+    void SetHorizontalVelocity()
+    {
+            m_HorizontalVelocity = m_WalkSpeedValue; 
+
+            if (m_IsRunning && m_IsWalking != 0) 
+            {
+                m_HorizontalVelocity += m_RunSpeedValue;
+                m_PlayerAnimator.speed = 1.5f;
+            }
+            else
+            {
+                m_PlayerAnimator.speed = 1f;
+            }
+
+            m_HorizontalVelocity *= m_IsWalking;
     }
 
     void Jump()
     {
         if (m_IsJumping == true)
         {
-            if (m_IsRunning && m_IsMoving)
+            if (m_IsRunning && m_IsWalking !=0)
             {
-                m_PlayerRigidbody.velocity = new Vector2(m_PlayerRigidbody.velocity.x, m_JumpSpeed * m_RunJumpBoost);
+                m_PlayerRigidbody.velocity = new Vector2(m_PlayerRigidbody.velocity.x, m_JumpSpeedValue * m_RunJumpBoost);
             }
-            else if (m_IsMoving)
+            else if (m_IsWalking != 0)
             {
-                m_PlayerRigidbody.velocity = new Vector2(m_PlayerRigidbody.velocity.x, m_JumpSpeed * m_WalkJumpBoost);
+                m_PlayerRigidbody.velocity = new Vector2(m_PlayerRigidbody.velocity.x, m_JumpSpeedValue * m_WalkJumpBoost);
             }
             else
             {
-                m_PlayerRigidbody.velocity = new Vector2(m_PlayerRigidbody.velocity.x, m_JumpSpeed);
+                m_PlayerRigidbody.velocity = new Vector2(m_PlayerRigidbody.velocity.x, m_JumpSpeedValue);
             }
 
         }
