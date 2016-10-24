f1d9dd2e7867feb37760508a7adb0b32cc47e7f6
Alberto-vgdd <alberto.vgdd@gmail.com>
2016-10-24 16:01:34 +0200

    World 1-1 is almost here.
    -Added tutorial level sprites
    -Tweaks on PlayerMovementScript and WormScript
    -Illumination system with normal maps is done.

--- Assets/Scripts/WormScript.cs
+++ Assets/Scripts/WormScript.cs
@@ -4,23 +4,6 @@ using System.Collections;
 
 
 
-//IMPORTANT:
-//      Alberto has changed Edit->Project Settings->Physics2D->Minimun Penetration Radious. Because it was creating gaps between colliders2D. The value was 0,01. Not it is 0,0025
-//      
-
-//TO DO:
-//      Make worms stay on platforms
-//
-//
-
-
-//CHANGES MADE BY ALBERTO:
-//     Worms do not flip their sprite now, because normal maps made some glithces
-//     Worm will change its direction if it collides with a "Entity" (Check Layers)
-//     Worm supports now SUPER MEGA ULTRA SLOW MOTION 
-//         
-
-
 
 public class WormScript : MonoBehaviour
 {
@@ -33,20 +16,20 @@ public class WormScript : MonoBehaviour
 
 
     private Rigidbody2D m_WormRigidbody;
-    private Collider2D m_WormCollider;
-    private SpriteRenderer m_WormSprite;
-    
-	
-	void Start ()
+    private BoxCollider2D m_WormCollider;
+    private SpriteRenderer m_WormSpriteRenderer;
+
+
+    void Start ()
     {
         //Variables initialization
         m_WormSpeed = 15f;
         m_WormRigidbody = gameObject.GetComponent<Rigidbody2D>(); 
-        m_WormCollider = gameObject.GetComponent<Collider2D>();
-        m_WormSprite = gameObject.GetComponent<SpriteRenderer>();
+        m_WormCollider = gameObject.GetComponent<BoxCollider2D>();
+        m_WormSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 
         //"transform.position - gameObject.GetComponent<Collider2D>().bounds.extents" calls the raycast at the end of Worm's collider.
-        RaycastHit2D RayHit =Physics2D.Raycast(transform.position - m_WormCollider.bounds.extents, Vector2.down, Mathf.Infinity);
+        RaycastHit2D RayHit = Physics2D.Raycast(transform.position - m_WormCollider.bounds.extents, Vector2.down, Mathf.Infinity);
 
         // Check if there is any platform down the enemy.
         if (RayHit.collider != null)
@@ -72,6 +55,7 @@ public class WormScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate ()
    {
+        ChangeColliderSize();
       if ((((transform.position.x + m_WormCollider.bounds.extents.x) >= (m_PlatformCenter.x + m_PlatformLength)) && m_FacingRight) || (((transform.position.x - m_WormCollider.bounds.extents.x) <= (m_PlatformCenter.x - m_PlatformLength)) && !m_FacingRight))
       {
             ChangeMovementDirection();
@@ -85,6 +69,12 @@ public class WormScript : MonoBehaviour
     {
         m_FacingRight = !m_FacingRight;
         m_WormSpeed *= -1;
+    }
+
+    void ChangeColliderSize()
+    {
+        //Set sprite's size as collider's size. You have to multiply the inversed local scale, otherwise the local scale will be applied again in the collider.
+        m_WormCollider.size = new Vector2(m_WormSpriteRenderer.bounds.size.x * 1f / transform.localScale.x, m_WormSpriteRenderer.bounds.size.y * 1f / transform.localScale.y);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
