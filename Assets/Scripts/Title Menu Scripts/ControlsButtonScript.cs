1cd2f77cbc1e767dc6b3a7f6306877751035d608
Alberto-vgdd <alberto.vgdd@gmail.com>
2016-11-23 20:56:29 +0100

    Tittle Menu added. WIP

---
+++ Assets/Scripts/Title Menu Scripts/ControlsButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControlsButtonScript : MonoBehaviour
+{
+    private Button m_ControlsButton;
+    public Sprite[] m_ButtonSprites;
+
+
+    void Start()
+    {
+        m_ControlsButton = GetComponent<Button>();
+    }
+
+    public void ButtonHover()
+    {
+        m_ControlsButton.image.overrideSprite = m_ButtonSprites[1];
+    }
+
+    public void ButtonNotHover()
+    {
+        m_ControlsButton.image.overrideSprite = m_ButtonSprites[0];
+    }
+
+    public void OpenControlsMenu()
+    {
+        SceneManager.LoadScene("L01");
+    }
+}
---
+++ Assets/Scripts/Title Menu Scripts/StartNewGameButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartNewGameButtonScript : MonoBehaviour
+{
+    private Button m_StartNewGameButton;
+    public Sprite[] m_ButtonSprites;
+
+
+    void Start()
+    {
+        m_StartNewGameButton = GetComponent<Button>();
+    }
+
+     public void ButtonHover()
+    {
+        m_StartNewGameButton.image.overrideSprite = m_ButtonSprites[1];
+    }
+
+    public void ButtonNotHover()
+    {
+        m_StartNewGameButton.image.overrideSprite = m_ButtonSprites[0];
+    }
+
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene("L01");
+    }
+   
+}
