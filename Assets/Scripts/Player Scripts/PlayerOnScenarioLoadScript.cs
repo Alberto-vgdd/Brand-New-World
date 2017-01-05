a540533258f8b63abba52e0818fa3b30f5d6965f
Alberto <alberto.vgdd@gmail.com>
2017-01-05 23:19:36 +0100

    Scenarios 1-8 joined

--- Assets/Scripts/Player Scripts/PlayerOnScenarioLoadScript.cs
+++ Assets/Scripts/Player Scripts/PlayerOnScenarioLoadScript.cs
@@ -11,7 +11,13 @@ public class PlayerOnScenarioLoadScript : MonoBehaviour
     private Vector3 m_RoomEntrance5 = new Vector3(8.269f, 2.729f, 0f);
     private Vector3 m_RoomEntrance6 = new Vector3(-5.993f, 1.011f, 0f);
     private Vector3 m_RoomEntrance7 = new Vector3(12.984f, 1.17f, 0f);
-    private Vector3 m_RoomEntrance99 = new Vector3(-9.78f, 6.74f, 0f);
+    private Vector3 m_RoomEntrance8 = new Vector3(-5.285f, -0.75f, 0f);
+    private Vector3 m_RoomEntrance9 = new Vector3(5.33f, -0.69f, 0f);
+    private Vector3 m_RoomEntrance10 = new Vector3(-6.63f, -0.325f, 0f);
+    private Vector3 m_RoomEntrance11 = new Vector3(3.679f, 1.026f, 0f);
+    private Vector3 m_RoomEntrance12 = new Vector3(-7.6f, 1.51f, 0f);
+    private Vector3 m_RoomEntrance13 = new Vector3(0.58f, -3.192f, 0f);
+    private Vector3 m_RoomEntrance14 = new Vector3(-9.78f, 6.74f, 0f);
 
     // Use this for initialization
     void Awake ()
@@ -42,8 +48,26 @@ public class PlayerOnScenarioLoadScript : MonoBehaviour
             case 7:
                 transform.position = m_RoomEntrance7;
                 break;
-            case 99:
-                transform.position = m_RoomEntrance99;
+            case 8:
+                transform.position = m_RoomEntrance8;
+                break;
+            case 9:
+                transform.position = m_RoomEntrance9;
+                break;
+            case 10:
+                transform.position = m_RoomEntrance10;
+                break;
+            case 11:
+                transform.position = m_RoomEntrance11;
+                break;
+            case 12:
+                transform.position = m_RoomEntrance12;
+                break;
+            case 13:
+                transform.position = m_RoomEntrance13;
+                break;
+            case 14:
+                transform.position = m_RoomEntrance14;
                 break;
         }
 
